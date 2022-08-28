a18fcd950125f3b0b27df9cb556fc367598cb6e5
Leah <lselig444@gmail.com>
2022-08-28 14:38:48 -0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -11,35 +11,3 @@ root.render(<App />);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-/* function Spa() {
-  return (
-    <HashRouter>
-      <NavBar />
-      <UserContext.Provider
-        value={{
-          users: [
-            {
-              name: "abel",
-              email: "abel@mit.edu",
-              password: "secret",
-              balance: 100,
-            },
-          ],
-        }}
-      >
-        <div className="container" style={{ padding: "20px" }}>
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/alldata/" component={AllData} />
-        </div>
-      </UserContext.Provider>
-    </HashRouter>
-  );
-}
-
-ReactDOM.render(<Spa />, document.getElementById("root"));
- */
